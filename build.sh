2ceeb3c42e1c07690f61f1ba779677fe17043a07
aagupta <aakriti@imanyco.com>
2023-03-17 16:25:05 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # This script installs all of the necessary dependencies
-
 apt-get update apt-get install libsndfile1 libportaudio2 -y
 pip install torch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
