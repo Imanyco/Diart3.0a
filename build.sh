8af3e4af40123cd47823f8622561fc28e126b703
aagupta <aakriti@imanyco.com>
2023-03-17 16:23:18 -0700

    Create build.sh
    create: shell script for DigitalOcean deployment

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script installs all of the necessary dependencies
+
+apt-get update apt-get install libsndfile1 libportaudio2 -y
+pip install torch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cpu
+pip install -r requirements.txt
+pip install python-socketio==5.4.0
+
+print('All dependencies installed successfully')
